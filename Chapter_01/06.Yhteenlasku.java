b8d04a423a8c354ba6934b1072dfed7ff46c5e01
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-05-11 19:11:25 +0300

    Add ch.01 - ex.06

---
+++ Chapter_01/06.Yhteenlasku.java
@@ -0,0 +1,16 @@
+public class Yhteenlasku {
+
+    public static void main(String[] args) {
+
+        // ÄLÄ KOSKE NÄIHIN
+        int a = 1337;
+        int b = 42;
+
+        // KORJAA TÄMÄ
+        int tulos = a + b;
+
+        // ÄLÄ KOSKE NÄIHIN
+        String tuloste = a + " + " + b + " = " + tulos;
+        System.out.println(tuloste);
+    }
+}
