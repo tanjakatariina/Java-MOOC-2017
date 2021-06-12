301de11744fe04425cec3728ad3e6b667d249846
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-06-12 17:24:03 +0300

    Add ch.01 - ex.24

---
+++ Chapter_01/24.ParitonVaiParillinen.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ParitonVaiParillinen {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        // Toteuta ohjelmasi tähän.
+        System.out.print("Anna luku: ");
+        int luku = Integer.parseInt(lukija.nextLine());
+
+        if ((luku % 2) == 0) {
+            System.out.println("Luku " + luku + " on parillinen.");
+        } else {
+            System.out.println("Luku " + luku + " on pariton.");
+        }
+    }
+}
