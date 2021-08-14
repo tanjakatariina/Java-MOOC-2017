a3e4c35b6cc3f933b818066623edb65c5183bff7
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-14 21:42:59 +0300

    Add ch.02 - ex.05

---
+++ Chapter_02/05.YhdestaSataan.java
@@ -0,0 +1,12 @@
+public class YhdestaSataan {
+
+    public static void main(String[] args) {
+        int luku = 1;
+        int loppuLuku = 100;
+
+        while (luku <= loppuLuku) {
+            System.out.println(luku);
+            luku++;
+        }
+    }
+}
