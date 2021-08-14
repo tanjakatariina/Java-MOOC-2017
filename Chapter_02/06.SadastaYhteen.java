d5a48d8c74b706caaa88d5400b8a32164739c2e4
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-14 21:50:55 +0300

    Add ch.02 - ex.06

---
+++ Chapter_02/06.SadastaYhteen.java
@@ -0,0 +1,12 @@
+public class SadastaYhteen {
+
+    public static void main(String[] args) {
+        int luku = 100;
+        int loppuLuku = 1;
+
+        while (luku >= loppuLuku) {
+            System.out.println(luku);
+            luku--;
+        }
+    }
+}
