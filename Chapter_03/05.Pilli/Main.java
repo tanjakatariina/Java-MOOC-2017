a0d3c1aba10ca64ab7f67fb876c925f1d191a5c0
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 12:42:01 +0300

    Add ch.03 - ex.05

---
+++ Chapter_03/05.Pilli/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Tämä on vain tyhjä main-metodi jossa voit kokeilla Pilli-luokkaasi.
+
+        Pilli sorsapilli = new Pilli("Kvaak");
+        Pilli kukkopilli = new Pilli("Peef");
+
+        sorsapilli.soi();
+        kukkopilli.soi();
+        sorsapilli.soi();
+    }
+}
---
+++ Chapter_03/05.Pilli/Pilli.java
@@ -0,0 +1,12 @@
+public class Pilli {
+
+    private String aani;
+
+    public Pilli(String pillinAani) {
+        this.aani = pillinAani;
+    }
+
+    public void soi() {
+        System.out.println(this.aani);
+    }
+}
