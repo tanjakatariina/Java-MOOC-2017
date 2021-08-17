7416fef6587fc217c5bf78c4ecc17ff494841399
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 12:39:10 +0300

    Add ch.03 - ex.04

---
+++ Chapter_03/04.ToinenLuokka/Luokka.java
@@ -0,0 +1,9 @@
+public class Luokka {
+    private String koodi;
+    private int istumapaikat;
+
+    public Luokka(String luokanKoodi, int istumapaikkojenMaara) {
+        this.koodi = luokanKoodi;
+        this.istumapaikat = istumapaikkojenMaara;
+    }
+}
