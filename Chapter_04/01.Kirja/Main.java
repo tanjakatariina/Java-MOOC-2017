be84044adefe08d6ea14b16b4b0af7a7bbafec0a
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 14:15:52 +0300

    Add ch.04 - ex.01

---
+++ Chapter_04/01.Kirja/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Kirja k = new Kirja("J. K. Rowling", "Harry Potter ja viisasten kivi", 223);
+
+        System.out.println(k);
+    }
+}
