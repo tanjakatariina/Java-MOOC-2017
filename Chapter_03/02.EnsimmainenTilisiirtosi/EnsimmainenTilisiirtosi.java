cfbb7b378bc6b54db77f73c220a5632b37c9d36d
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 12:24:45 +0300

    Add ch.03 - ex.02

---
+++ Chapter_03/02.EnsimmainenTilisiirtosi/EnsimmainenTilisiirtosi.java
@@ -0,0 +1,14 @@
+public class EnsimmainenTilisiirtosi {
+
+    public static void main(String[] args) {
+        // Tili.Java:ssa olevaan koodiin ei tule koskea
+        Tili matinTili = new Tili("Matin tili", 1000);
+        Tili omaTili = new Tili("Oma tili", 0);
+
+        matinTili.otto(100.0);
+        omaTili.pano(100.0);
+
+        System.out.println(matinTili);
+        System.out.println(omaTili);
+    }
+}
