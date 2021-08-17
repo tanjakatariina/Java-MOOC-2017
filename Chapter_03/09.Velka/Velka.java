655032007af0edb2ae3136d8d83b480d9edcc9a6
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 13:05:50 +0300

    Add ch.03 - ex.09

---
+++ Chapter_03/09.Velka/Velka.java
@@ -0,0 +1,18 @@
+public class Velka {
+
+    private double saldo;
+    private double korkokerroin;
+
+    public Velka(double saldoAlussa, double korkokerroinAlussa) {
+        this.saldo = saldoAlussa;
+        this.korkokerroin = korkokerroinAlussa;
+    }
+
+    public void tulostaSaldo() {
+        System.out.println(this.saldo);
+    }
+
+    public void odotaVuosi() {
+        this.saldo *= this.korkokerroin;
+    }
+}
