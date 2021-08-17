2d029fbf9ab48293509843045250e4d8186133f1
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 12:22:17 +0300

    Add ch.03 - ex.01

---
+++ Chapter_03/01.EnsimmainenTilisi/EnsimmainenTilisi.java
@@ -0,0 +1,12 @@
+public class EnsimmainenTilisi {
+
+    public static void main(String[] args) {
+        // Tili.Java:ssa olevaan koodiin ei tule koskea
+
+        Tili tanjanTili = new Tili("Tanjan tili", 100.0);
+
+        tanjanTili.pano(20.0);
+
+        System.out.println(tanjanTili);
+    }
+}
