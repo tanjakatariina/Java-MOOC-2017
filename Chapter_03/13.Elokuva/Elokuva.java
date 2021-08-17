bb63f545d25320ebaa39a2f14ec3a9a2481a49da
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 13:19:39 +0300

    Add ch.03 - ex.13

---
+++ Chapter_03/13.Elokuva/Elokuva.java
@@ -0,0 +1,18 @@
+public class Elokuva {
+
+    private String nimi;
+    private int ikaraja;
+
+    public Elokuva(String elokuvanNimi, int elokuvanIkaraja) {
+        this.nimi = elokuvanNimi;
+        this.ikaraja = elokuvanIkaraja;
+    }
+
+    public String nimi() {
+        return this.nimi;
+    }
+
+    public int ikaraja() {
+        return this.ikaraja;
+    }
+}
---
+++ Chapter_03/13.Elokuva/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Tämä on vain tyhjä main-metodi jossa voit kokeilla Elokuva-luokkaasi.
+
+        Elokuva chipmunks = new Elokuva("Alvin and the Chipmunks: The Squeakquel", 0);
+
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Minkä ikäinen olet?");
+        int ika = Integer.parseInt(lukija.nextLine());
+
+        System.out.println();
+        if (ika >= chipmunks.ikaraja()) {
+            System.out.println("Saat katsoa elokuvan " + chipmunks.nimi());
+        } else {
+            System.out.println("Et saa katsoa elokuvaa " + chipmunks.nimi());
+        }
+    }
+}
