b01e0bf150afa0fe6025c700e10713237e7208f8
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-08-17 12:45:50 +0300

    Add ch.03 - ex.06

---
+++ Chapter_03/06.Ovi/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Tämä on vain tyhjä main-metodi jossa voit kokeilla Ovi-luokkaasi.
+
+        Ovi alexander = new Ovi();
+
+        alexander.koputa();
+        alexander.koputa();
+    }
+}
---
+++ Chapter_03/06.Ovi/Ovi.java
@@ -0,0 +1,9 @@
+public class Ovi {
+
+    public Ovi() {
+    }
+
+    public void koputa() {
+        System.out.println("Who's there?");
+    }
+}
