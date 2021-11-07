3cc9729886f00afa33df5f41afa1a2d60a07eda8
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-11-07 18:49:27 +0200

    Add ch.04 - ex.08

---
+++ Chapter_04/08.Kasvatuslaitos/Paaohjelma.java
@@ -0,0 +1,23 @@
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        Kasvatuslaitos haaganNeuvola = new Kasvatuslaitos();
+
+        Henkilo eero = new Henkilo("Eero", 1, 110, 7);
+        Henkilo pekka = new Henkilo("Pekka", 33, 176, 85);
+
+        System.out.println("punnituksia tehty " + haaganNeuvola.punnitukset());
+
+        haaganNeuvola.punnitse(eero);
+        haaganNeuvola.punnitse(pekka);
+
+        System.out.println("punnituksia tehty " + haaganNeuvola.punnitukset());
+
+        haaganNeuvola.punnitse(eero);
+        haaganNeuvola.punnitse(eero);
+        haaganNeuvola.punnitse(eero);
+        haaganNeuvola.punnitse(eero);
+
+        System.out.println("punnituksia tehty " + haaganNeuvola.punnitukset());
+    }
+}
