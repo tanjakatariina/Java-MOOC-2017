d2b2a926b354bd9a34a96d58dc6176dbff78cc6a
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-11-07 18:52:00 +0200

    Add ch.04 - ex.09

---
+++ Chapter_04/09.MaksukorttiJaKassapaate/Maksukortti.java
@@ -0,0 +1,25 @@
+public class Maksukortti {
+
+    private double saldo;
+
+    public Maksukortti(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double saldo() {
+        return this.saldo;
+    }
+
+    public void lataaRahaa(double lisays) {
+        this.saldo += lisays;
+    }
+
+    public boolean otaRahaa(double maara) {
+        if (saldo >= maara) {
+            this.saldo -= maara;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
